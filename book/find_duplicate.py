08550579221bf96ae3af38f28c3c3f61fc54d998
borko81 <bstoilov81@gmail.com>
2021-09-18 15:03:46 +0300

    Add find duplicates and some examples

--- book/find_duplicate.py
+++ book/find_duplicate.py
@@ -1,1 +1,41 @@
-print('test')
+from collections import Counter
+
+a = [1, 7, 2, 9, 7, 11]
+
+
+def find_fuplicates_in_list(items):
+    found_items = set()
+    for item in items:
+        if item not in found_items:
+            yield item
+            found_items.add(item)
+
+
+# print(list(find_fuplicates_in_list(a)))
+
+# Found most looked element
+# First using Counter
+# print(Counter(a))
+
+# Use dict
+data = {x: a.count(x) for x in a}
+print(dict(sorted(data.items(), key=lambda x: -x[1])))
+
+
+# Sorted instance of classes
+
+class User:
+
+    def __init__(self, user_id) -> None:
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} has id: {str(self.user_id)}"
+
+
+a1 = User(101)
+a2 = User(100)
+a3 = User(99)
+
+users = [a1, a2, a3]
+print(sorted(users, key=lambda x: x.user_id))
